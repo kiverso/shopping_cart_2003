fc1836b550eb87275c7f11f80d3483ae6c3af7cc
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-03-25 10:07:23 -0600

    Add total_number_of_items_attribute and test

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -1,13 +1,14 @@
 class ShoppingCart
-  attr_reader :name, :capacity, :products
+  attr_reader :name, :capacity, :products, :total_number_of_products
   def initialize(name, capacity, products = [])
     @name = name
     @capacity = capacity.to_i
     @products = products
+    @total_number_of_products = 0
   end
 
   def add_product(product)
     @products << product
+    @total_number_of_products += product.quantity
   end
-
 end
--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -35,4 +35,17 @@ class ShoppingCartTest < Minitest::Test
     cart.add_product(product2)
     assert_equal [product1, product2], cart.products
   end
+
+  def test_it_can_get_total_number_of_products
+    cart = ShoppingCart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+
+    assert_equal 13, cart.total_number_of_products
+  end
 end
